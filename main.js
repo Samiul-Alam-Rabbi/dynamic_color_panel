291c3f1b311e8c49349284342bbc148579623bee
msar3469 <msar3469@gmail.com>
2021-03-14 16:52:43 +0600

    colorpanel

--- main.js
+++ main.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $('.control-btn').click(function(){
         $('#panel-box').toggleClass('controlbtnactive');
     })
+    jQuery('.red-color').click(function(){
+        jQuery('body').addClass('red-color-bar').removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    })
+    jQuery('.green-color').click(function(){
+        jQuery('body').addClass('green-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    })
+    jQuery('.yellow-color').click(function(){
+        jQuery('body').addClass('yellow-color-bar').removeClass('green-color-bar').removeClass('red-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    })
+    jQuery('.violet-color').click(function(){
+        jQuery('body').addClass('violet-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('green-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    })
+    jQuery('.cyan-color').click(function(){
+        jQuery('body').addClass('cyan-color-bar').removeClass('green-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('blue-color-bar')
+    })
+    jQuery('.blue-color').click(function(){
+        jQuery('body').addClass('blue-color-bar').removeClass('red-color-bar').removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('cyan-color-bar').removeClass('violet-color-bar')
+    })
 })
