a81e743f5356ad21d12a76bba69c0651694eb0c3
msar3469 <msar3469@gmail.com>
2021-03-15 08:09:35 +0600

    pushit

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ $(document).ready(function(){
     $('.control-btn').click(function(){
         $('#panel-box').toggleClass('controlbtnactive');
     })
-    jQuery('.red-color').click(function(){
-        jQuery('body').addClass('red-color-bar').removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    $('.red-color').click(function(){
+        $('body').addClass('red-color-bar').css({"color": "red", "font-size": "24px"}).removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
     })
-    jQuery('.green-color').click(function(){
-        jQuery('body').addClass('green-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    $('.green-color').click(function(){
+        $('body').addClass('green-color-bar').css({"color": "green", "font-size": "24px"}).removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
     })
-    jQuery('.yellow-color').click(function(){
-        jQuery('body').addClass('yellow-color-bar').removeClass('green-color-bar').removeClass('red-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    $('.yellow-color').click(function(){
+        $('body').addClass('yellow-color-bar').css({"color": "yellow", "font-size": "24px"}).removeClass('green-color-bar').removeClass('red-color-bar').removeClass('violet-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
     })
-    jQuery('.violet-color').click(function(){
-        jQuery('body').addClass('violet-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('green-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
+    $('.violet-color').click(function(){
+        $('body').addClass('violet-color-bar').css({"color": "violet", "font-size": "24px"}).removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('green-color-bar').removeClass('cyan-color-bar').removeClass('blue-color-bar')
     })
-    jQuery('.cyan-color').click(function(){
-        jQuery('body').addClass('cyan-color-bar').removeClass('green-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('blue-color-bar')
+    $('.cyan-color').click(function(){
+        $('body').addClass('cyan-color-bar').css({"color": "cyan", "font-size": "24px"}).removeClass('green-color-bar').removeClass('red-color-bar').removeClass('yellow-color-bar').removeClass('violet-color-bar').removeClass('blue-color-bar')
     })
-    jQuery('.blue-color').click(function(){
-        jQuery('body').addClass('blue-color-bar').removeClass('red-color-bar').removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('cyan-color-bar').removeClass('violet-color-bar')
+    $('.blue-color').click(function(){
+        $('body').addClass('blue-color-bar').css({"color": "blue", "font-size": "24px"}).removeClass('red-color-bar').removeClass('green-color-bar').removeClass('yellow-color-bar').removeClass('cyan-color-bar').removeClass('violet-color-bar')
     })
 })
